1ccbafe8adbc3005fd4e40e24820c4bbc13754a0
Balbindar Singh <basingh@toryburch.com>
2026-02-26 22:51:31 +0530

    Initial Playwright E2E setup

---
+++ login_logout.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://com.stg.aem.toryburch.com/auth-form.html?redirect=/en-us/');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('storefront');
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('torydev');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.getByRole('button', { name: 'My Account' }).hover();
+  await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('textbox', { name: 'Email Address' }).click();
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('basingh@destm.com');
+  await page.getByRole('textbox', { name: 'Email Address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'show password Password' }).fill('Khalsa@19841699');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.getByRole('button', { name: 'My Account' }).hover();
+  await expect(page.getByRole('button', { name: 'My Account' })).toBeVisible();
+  await page.getByLabel('My Account').click();
+  await page.getByRole('button', { name: 'Sign Out' }).click();
+  await page.getByRole('button', { name: 'My Account' }).hover();
+  await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
+});
